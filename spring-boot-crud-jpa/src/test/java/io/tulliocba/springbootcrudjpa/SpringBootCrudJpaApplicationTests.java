96b1101a3b03e510ecfaa3390b533d0d3fc8237e
Tulio Gabriel <tullio_cba@hotmail.com>
2019-09-01 22:07:32 -0300

    Start Project

---
+++ spring-boot-crud-jpa/src/test/java/io/tulliocba/springbootcrudjpa/SpringBootCrudJpaApplicationTests.java
@@ -0,0 +1,16 @@
+package io.tulliocba.springbootcrudjpa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootCrudJpaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
