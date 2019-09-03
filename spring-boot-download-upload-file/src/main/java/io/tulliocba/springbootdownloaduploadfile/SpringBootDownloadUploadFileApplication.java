93142612eb105557da5f3a8d675e7d93e34c20fc
Tulio Gabriel <tullio_cba@hotmail.com>
2019-09-03 18:55:21 -0300

    Project created

---
+++ spring-boot-download-upload-file/src/main/java/io/tulliocba/springbootdownloaduploadfile/SpringBootDownloadUploadFileApplication.java
@@ -0,0 +1,13 @@
+package io.tulliocba.springbootdownloaduploadfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDownloadUploadFileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDownloadUploadFileApplication.class, args);
+    }
+
+}
---
+++ spring-boot-download-upload-file/src/test/java/io/tulliocba/springbootdownloaduploadfile/SpringBootDownloadUploadFileApplicationTests.java
@@ -0,0 +1,16 @@
+package io.tulliocba.springbootdownloaduploadfile;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootDownloadUploadFileApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
