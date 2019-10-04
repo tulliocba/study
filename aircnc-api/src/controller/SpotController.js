2bd785a8d184d0e0d7a39253b78c613a7864df20
Tulio Gabriel <tullio_cba@hotmail.com>
2019-10-04 07:43:25 -0300

    Finalizado aula de backend

--- aircnc-api/src/controller/SpotController.js
+++ aircnc-api/src/controller/SpotController.js
@@ -2,14 +2,17 @@ const Spot = require('../model/Spot');
 const User = require('../model/User');
 
 module.exports = {
+    async index(req, res) {
+        const { tech } = req.query;
+        const spots = await Spot.find({ techs: tech });
+        return res.json(spots);
+    },
+
     async store(req, res) {
         const { filename } = req.file;
         const { company, techs, price } = req.body;
         const { user_id } = req.headers;
-
         const user = await User.findById(user_id);
-
-        console.log(`${user} ==> Usuário`);
 
         if (!user) {
             return res.status(400).json({ error: 'User does not exists' });
--- aircnc-api/src/routes.js
+++ aircnc-api/src/routes.js
@@ -5,12 +5,13 @@ const uploadConfig = require('./config/Upload');
 
 const SessionController = require('./controller/SessionController');
 const SpotController = require('./controller/SpotController');
+const BookingController = require('./controller/BookingController');
 
 const routes = express.Router();
 const upload = multer(uploadConfig);
 
 routes.post('/users', SessionController.store);
-
+routes.get('/spots', SpotController.index);
 routes.post('/spots', upload.single('thumbnail'), SpotController.store);
-
+routes.post('/spots/:spot_id/bookings', BookingController.store);
 module.exports = routes;
