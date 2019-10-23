578a72942c63a91c899dab345cc5ebd7120bc936
Tulio Gabriel <tullio_cba@hotmail.com>
2019-10-23 07:57:09 -0300

    Configurando o multer para upload de imagem

--- bootcamp2019/gobarber-api/src/app/controllers/UserController.js
+++ bootcamp2019/gobarber-api/src/app/controllers/UserController.js
@@ -1,8 +1,24 @@
 /* eslint-disable class-methods-use-this */
+import * as Yup from 'yup';
+
 import User from '../models/User';
 
 class UserController {
     async store(req, res) {
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            email: Yup.string()
+                .email()
+                .required(),
+            password: Yup.string()
+                .required()
+                .min(6),
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Validation fails' });
+        }
+
         const userExists = await User.findOne({
             where: { email: req.body.email },
         });
@@ -20,6 +36,26 @@ class UserController {
     }
 
     async update(req, res) {
+        const schema = Yup.object().shape({
+            name: Yup.string(),
+            email: Yup.string().email(),
+            oldPassword: Yup.string()
+                .required()
+                .min(6),
+            password: Yup.string()
+                .min(6)
+                .when('oldPassword', (oldPassword, field) =>
+                    oldPassword ? field.required() : field
+                ),
+            confirmPassword: Yup.string().when('password', (password, field) =>
+                password ? field.required().oneOf([Yup.ref('password')]) : field
+            ),
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Validation fails' });
+        }
+
         const { userId } = req;
 
         const { email, oldPassword } = req.body;
--- bootcamp2019/gobarber-api/src/routes.js
+++ bootcamp2019/gobarber-api/src/routes.js
@@ -1,14 +1,20 @@
 // importo somente o componente Router do express.
 import { Router } from 'express';
 
+import multer from 'multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
+import multerConfig from './config/multer';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+routes.post('/files', upload.single('file'), (req, res) => {
+    return res.json({ ok: true });
+});
 
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
