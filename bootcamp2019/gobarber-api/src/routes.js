de039491a77247f3375e94bd8f97863cd0a10b60
Tulio Gabriel <tullio_cba@hotmail.com>
2019-10-24 09:18:52 -0300

    Criado a listagem de providers

--- bootcamp2019/gobarber-api/src/routes.js
+++ bootcamp2019/gobarber-api/src/routes.js
@@ -4,18 +4,20 @@ import { Router } from 'express';
 import multer from 'multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 import authMiddleware from './app/middlewares/auth';
 import multerConfig from './config/multer';
-import FileController from './app/controllers/FileController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
-routes.post('/files', upload.single('file'), FileController.store);
 
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
+routes.get('/providers', ProviderController.index);
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
