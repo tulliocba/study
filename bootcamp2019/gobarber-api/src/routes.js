71178457ee1f7e17b955e2d1175ce78357ca785e
Tulio Gabriel <tullio_cba@hotmail.com>
2019-10-29 07:04:03 -0300

    Criado o endpoint schedule

--- bootcamp2019/gobarber-api/src/routes.js
+++ bootcamp2019/gobarber-api/src/routes.js
@@ -1,14 +1,16 @@
 // importo somente o componente Router do express.
 import { Router } from 'express';
-
 import multer from 'multer';
+
+import authMiddleware from './app/middlewares/auth';
+import multerConfig from './config/multer';
+
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
-import authMiddleware from './app/middlewares/auth';
-import multerConfig from './config/multer';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -21,6 +23,7 @@ routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
+routes.get('/schedules', ScheduleController.index);
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
