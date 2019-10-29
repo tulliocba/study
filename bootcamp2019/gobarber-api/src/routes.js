b3d293cfba1514a4f5c37f36d550887e958f4e03
Tulio Gabriel <tullio_cba@hotmail.com>
2019-10-29 09:30:50 -0300

    Criado endpoint para leitura de notificação

--- bootcamp2019/gobarber-api/src/routes.js
+++ bootcamp2019/gobarber-api/src/routes.js
@@ -11,6 +11,7 @@ import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -24,6 +25,8 @@ routes.get('/providers', ProviderController.index);
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
 routes.get('/schedules', ScheduleController.index);
+routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
