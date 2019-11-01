ae43ba5faccdd8a6737b7cc7dbc7c108896a5061
Tulio Gabriel <tullio_cba@hotmail.com>
2019-11-01 10:53:09 -0300

    Last implementation of Equilibrium

--- challanges/src/main/java/com/github/tulliocba/equilibrium/Equilibrium.java
+++ challanges/src/main/java/com/github/tulliocba/equilibrium/Equilibrium.java
@@ -1,63 +1,25 @@
 package com.github.tulliocba.equilibrium;
 
 
-import lombok.Getter;
-
 public class Equilibrium {
-    @Getter
-    private int point;
-    @Getter
-    private int absDiff;
 
-    public Equilibrium(int point, int absDiff) {
-        this.point = point;
-        this.absDiff = absDiff;
+    public static int solution(int[] A) {
+        int left = A[0];
+        int right = sumLeftToRight(1, A);
+        int diff = Math.abs(left - right);
+
+        for (int i = 1; i < A.length - 1; i++) {
+            left += A[i];
+            right -= A[i];
+            int currentDiff = Math.abs(left - right);
+            if (currentDiff < diff) diff = currentDiff;
+        }
+        return diff;
     }
 
-    static Equilibrium findEquilibrium(int[] n) {
-        int absDiff = 0;
-        int point = 0;
-
-        for (int i = 1; i < n.length; i++) { // {1, 2, 3}; i = 1; value = 2;
-            int left = 0;
-            int right = 0;
-            for (int j = 0; j < i; j++) { // j = 0; value = 1;
-                left += n[j];
-            }
-            for (int j = n.length - 1; j >= i; j--) {
-                right += n[j];
-            }
-
-            int abs = left - right;
-            abs = Math.abs(abs);
-            if(i == 1) {
-                absDiff = abs;
-            }
-            if(abs < absDiff) {
-                absDiff = abs;
-                point = i;
-            }
-
-        }
-        return new Equilibrium(point, absDiff);
-    }
-
-    public static int findEquilibrium2(int[] n) {
-        int right = 0;
-        int left = 0;
-        int absDiff = 0;
-        for (int i = 0; i < n.length; i++) right += n[i];
-
-        absDiff = Math.abs(n[0] - right);
-
-        for (int i = 1; i < n.length; i++) {
-            left += n[i-1];
-            right = (right - n[i-1]);
-            int abs = Math.abs(left - right);
-            if(abs < absDiff) {
-                absDiff = abs;
-            }
-        }
-        return absDiff;
+    private static int sumLeftToRight(int initIndex, int[] A) {
+        int result = 0;
+        for (int i = initIndex; i < A.length; i++) result += A[i];
+        return result;
     }
 }
--- challanges/src/test/java/com/github/tulliocba/equilibrium/EquilibriumTest.java
+++ challanges/src/test/java/com/github/tulliocba/equilibrium/EquilibriumTest.java
@@ -1,22 +1,25 @@
 package com.github.tulliocba.equilibrium;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class EquilibriumTest {
-    int[] n = new int[]{3, 1, 2, 4, 3};
+    int[] n;
 
-    @Test
-    public void find_the_equilibrium_test() {
-        Equilibrium equilibrium = Equilibrium.findEquilibrium(n);
-        assertThat(equilibrium.getPoint()).isEqualTo(3);
-        assertThat(equilibrium.getAbsDiff()).isEqualTo(1);
+    @Before
+    public void setUp() {
+        n = new int[201];
+        int point = 0;
+        for (int i = -100; i <= 100; i++) {
+            n[point++] = i;
+        }
     }
 
     @Test
     public void find_the_equilibrium2_test() {
-        int equilibrium = Equilibrium.findEquilibrium2(n);
-        assertThat(equilibrium).isEqualTo(1);
+        int equilibrium = Equilibrium.solution(n);
+        assertThat(equilibrium).isEqualTo(200);
     }
 }
