a8b0fea61d00fc87248bb25061d09618bce526f2
Tulio Gabriel <tullio_cba@hotmail.com>
2019-11-07 14:05:43 -0300

    Criado serverconfig

---
+++ microservice-lab/lab-config-server/src/test/java/com/github/tulliocba/labconfigserver/LabConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.tulliocba.labconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
