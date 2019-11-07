ee39951e152427da8f30c534cd452fe391eddb8d
Tulio Gabriel <tullio_cba@hotmail.com>
2019-11-07 16:16:25 -0300

    Created config client

---
+++ microservice-lab/client-config-server/src/main/java/com/github/tulliocba/clientconfigserver/ClientConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.tulliocba.clientconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientConfigServerApplication.class, args);
+    }
+
+}
---
+++ microservice-lab/client-config-server/src/test/java/com/github/tulliocba/clientconfigserver/ClientConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.tulliocba.clientconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
