c674338d654740acd37c484b837c7f6a2b3f09c7
Tulio Gabriel <tullio_cba@hotmail.com>
2019-11-15 10:15:34 -0300

    Corrigido as rotas

--- bootcamp2019/module06/android/app/src/main/java/com/module06/MainActivity.java
+++ bootcamp2019/module06/android/app/src/main/java/com/module06/MainActivity.java
@@ -15,14 +15,13 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "module06";
   }
-
-  @Override
-  protected ReactActivityDelegate createReactActivityDelegate() {
-    return new ReactActivityDelegate(this, getMainComponentName()) {
-      @Override
-      protected ReactRootView createRootView() {
-       return new RNGestureHandlerEnabledRootView(MainActivity.this);
-      }
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+        return new ReactActivityDelegate(this, getMainComponentName()) {
+        @Override
+        protected ReactRootView createRootView() {
+            return new RNGestureHandlerEnabledRootView(MainActivity.this);
+        }
     };
   }
 }
