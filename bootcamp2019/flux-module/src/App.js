30d96d0a9bc9c3d84fd628f71492a7042b502299
Tulio Gabriel <tullio_cba@hotmail.com>
2019-11-28 09:07:56 -0300

    Adicionando produto

--- bootcamp2019/flux-module/src/App.js
+++ bootcamp2019/flux-module/src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import GlobalStyles from "./styles/global";
+import "./config/ReactotronConfig";
 import Header from "./components/Header";
-
 import Routes from "./routes";
 import store from "./store";
 
--- bootcamp2019/flux-module/src/pages/Cart/index.js
+++ bootcamp2019/flux-module/src/pages/Cart/index.js
@@ -1,5 +1,6 @@
 /* eslint-disable react/button-has-type */
 import React from "react";
+import { connect } from "react-redux";
 import {
     MdRemoveCircleOutline,
     MdAddCircleOutline,
@@ -7,7 +8,7 @@ import {
 } from "react-icons/md";
 import { Container, ProductTable, Total } from "./styles";
 
-export default function Cart() {
+function Cart({ cart }) {
     return (
         <Container>
             <ProductTable>
@@ -21,38 +22,46 @@ export default function Cart() {
                     </tr>
                 </thead>
                 <tbody>
-                    <td>
-                        <img
-                            src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/26/HZM-1731-026/HZM-1731-026_detalhe2.jpg?ims=326x"
-                            alt="Tênis"
-                        />
-                    </td>
-                    <td>
-                        <strong>Tênis muito massa</strong>
-                        <span>R$129,90</span>
-                    </td>
-                    <td>
-                        <div>
-                            <button type="button ">
-                                <MdRemoveCircleOutline
-                                    size={20}
-                                    color="#7159c1"
-                                />
-                            </button>
-                            <input type="number" readOnly value={2} />
-                            <button type="button ">
-                                <MdAddCircleOutline size={20} color="#7159c1" />
-                            </button>
-                        </div>
-                    </td>
-                    <td>
-                        <strong>R$258,80</strong>
-                    </td>
-                    <td>
-                        <button type="button">
-                            <MdDelete size={20} color="#7159c1" />
-                        </button>
-                    </td>
+                    {cart.map(product => (
+                        <tr>
+                            <td>
+                                <img src={product.image} alt={product.title} />
+                            </td>
+                            <td>
+                                <strong>{product.title}</strong>
+                                <span>{product.priceFormatted}</span>
+                            </td>
+                            <td>
+                                <div>
+                                    <button type="button ">
+                                        <MdRemoveCircleOutline
+                                            size={20}
+                                            color="#7159c1"
+                                        />
+                                    </button>
+                                    <input
+                                        type="number"
+                                        readOnly
+                                        value={product.amount}
+                                    />
+                                    <button type="button ">
+                                        <MdAddCircleOutline
+                                            size={20}
+                                            color="#7159c1"
+                                        />
+                                    </button>
+                                </div>
+                            </td>
+                            <td>
+                                <strong>R$258,80</strong>
+                            </td>
+                            <td>
+                                <button type="button">
+                                    <MdDelete size={20} color="#7159c1" />
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
                 </tbody>
             </ProductTable>
             <footer>
@@ -65,3 +74,9 @@ export default function Cart() {
         </Container>
     );
 }
+
+const mapStateToProps = state => ({
+    cart: state.cart
+});
+
+export default connect(mapStateToProps)(Cart);
