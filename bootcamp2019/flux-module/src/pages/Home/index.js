d94164d0a9db6af8524a393396e378b134456dc8
Tulio Gabriel <tullio_cba@hotmail.com>
2019-12-02 09:20:20 -0300

    Separando Actions

--- bootcamp2019/flux-module/src/pages/Home/index.js
+++ bootcamp2019/flux-module/src/pages/Home/index.js
@@ -22,9 +22,9 @@ class Home extends Component {
         this.setState({ produtcs: data });
     }
 
-    handleAddProduct = product => {
-        const { addToCart } = this.props;
-        addToCart(product);
+    handleAddProduct = id => {
+        const { addToCartRequest } = this.props;
+        addToCartRequest(id);
     };
 
     render() {
@@ -39,7 +39,7 @@ class Home extends Component {
                         <span>{product.priceFormatted}</span>
                         <button
                             type="button"
-                            onClick={() => this.handleAddProduct(product)}
+                            onClick={() => this.handleAddProduct(product.id)}
                         >
                             <div>
                                 <MdAddShoppingCart size={16} color="#FFF" />{" "}
