dc2f8f31e1d1bf4a09230052a0c472d0e2546e8f
Tulio Gabriel <tullio_cba@hotmail.com>
2019-12-09 09:23:53 -0300

    Global Styles

--- bootcamp2019/gobarber-web/src/pages/SignIn/index.js
+++ bootcamp2019/gobarber-web/src/pages/SignIn/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import logo from '../../assets/logo.svg';
+import {Link} from 'react-router-dom';
 
 export default function SignIn() {
-  return <h1>Login</h1>;
+  return (
+    <>
+      <img src={logo} alt="GoBarber"/>
+
+      <form>
+        <input type="email" placeholder="Seu e-mail"/>
+        <input type="password" placeholder="Sua senha"/>
+
+        <button type="submit">Acessar </button>
+        <Link to="/register">Criar conta gratuita</Link>
+      </form>
+    </>
+  );
 }
--- bootcamp2019/gobarber-web/src/pages/SignUp/index.js
+++ bootcamp2019/gobarber-web/src/pages/SignUp/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import logo from '../../assets/logo.svg';
+import {Link} from 'react-router-dom';
 
 export default function SignUp() {
   return (
-    <div />
+    <>
+      <img src={logo} alt="GoBarber"/>
+
+      <form>
+        <input placeholder="Nome completo"/>
+        <input type="email" placeholder="Seu e-mail"/>
+        <input type="password" placeholder="Sua senha"/>
+
+        <button type="submit">Criar conta </button>
+        <Link to="/register">Já tenho login</Link>
+      </form>
+    </>
   );
 }
